772b753383ed8103222b33a7adbb8173e4e5c2d8
ViperRedBulLs <vipercode305@gmail.com>
2024-03-17 01:57:03 +0700

    Root layout

---
+++ app/layout.tsx
@@ -0,0 +1,47 @@
+import { MyAlert } from "@/components/my-alert";
+import Navbar from "@/components/navbar";
+import ThemeProvider from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Poppins as FontSans } from "next/font/google";
+import NextTopLoader from "nextjs-toploader";
+import "yet-another-react-lightbox/plugins/captions.css";
+import "yet-another-react-lightbox/styles.css";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+
+  variable: "--font-sans",
+  weight: ["300", "400", "600", "800", "900"],
+});
+
+export const metadata: Metadata = {
+  title: "Kafe Koding",
+  description:
+    "Kafe Koding merupakan sebuah komunitas belajar yang bergerak di bidang IT dan juga tempat sharing ilmu programing dan seputar dunia IT.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="id">
+      <body className={cn(fontSans.variable)}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <NextTopLoader height={4} />
+          <Navbar />
+          {children}
+          <MyAlert />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
